b42367d34557147cfa4c57a4d1d120b849fd5657
Eduardo Derisso <dudu.derisso@gmail.com>
2024-09-09 21:11:29 -0300

    feat[MainActivity]: saving instance data

--- app/src/main/java/com/example/cdv/MainActivity.kt
+++ app/src/main/java/com/example/cdv/MainActivity.kt
@@ -3,6 +3,10 @@ package com.example.cdv
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.ViewGroup.LayoutParams.MATCH_PARENT
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
+import android.widget.EditText
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,9 +19,36 @@ class MainActivity : AppCompatActivity() {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
+    private companion object {
+        const val VALOR_TELEFONE_UM = "VALOR_TELEFONE_UM"
+        const val VALOR_TELEFONE_DOIS = "VALOR_TELEFONE_DOIS"
+    }
+
+    private val telefoneDoiEt: EditText by lazy {
+        EditText(this)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(amb.root)
+
+        telefoneDoiEt.hint = "Segundo Telefone"
+        telefoneDoiEt.layoutParams = LinearLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT)
+        amb.root.addView(telefoneDoiEt)
+
+        savedInstanceState?.apply { // funções de escopo -> funções de alta ordem que facilitam a vida
+            getString(VALOR_TELEFONE_UM)?.let { // garante que a variavel não é nula
+                amb.telefoneEt.setText(it)
+            }
+            getString(VALOR_TELEFONE_DOIS, "00000000").let{
+                telefoneDoiEt.setText(it)
+            }
+            Log.v(
+                getString(R.string.app_name),
+                "onCreate: restaurando dados de instância"
+            )
+        }
+
 
         amb.abraBt.setOnClickListener {
             val anotherIntent: Intent = Intent(this, AnotherActivity::class.java).also{
@@ -27,6 +58,50 @@ class MainActivity : AppCompatActivity() {
 
         Log.v(getString(R.string.app_name), "onCreate: Iniciando ciclo completo")
     }
+
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(VALOR_TELEFONE_UM, amb.telefoneEt.text.toString())
+        outState.putString(VALOR_TELEFONE_DOIS, telefoneDoiEt.text.toString())
+        Log.v(
+            getString(R.string.app_name),
+            "onSavedInstanceState: Salvando dados de instância"
+        )
+    }
+
+
+//    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+//        super.onRestoreInstanceState(savedInstanceState)
+//
+//        //forma Java
+//        /*
+//        val valorTelefoneUm = savedInstanceState.getString(VALOR_TELEFONE_UM)
+//        if (valorTelefoneUm != null) {
+//            amb.telefoneEt.setText(valorTelefoneUm)
+//        }
+//
+//        val valorTelefoneDois = savedInstanceState.getString(VALOR_TELEFONE_DOIS)
+//        telefoneDoisEt.setText(valorTelefoneDois)
+//
+//
+//        * */
+//
+//        // forma Kotlin de programar
+//        savedInstanceState.apply { // funções de escopo -> funções de alta ordem que facilitam a vida
+//            getString(VALOR_TELEFONE_UM)?.let { // garante que a variavel não é nula
+//                amb.telefoneEt.setText(it)
+//            }
+//            getString(VALOR_TELEFONE_DOIS, "00000000").let{
+//                telefoneDoiEt.setText(it)
+//            }
+//        }
+//        Log.v(
+//            getString(R.string.app_name),
+//            "onRestoreInstanceState: restaurando dados de instância"
+//        )
+//    }
+
     override fun onStart() {
         super.onStart()
         Log.v(getString(R.string.app_name), "onStart: Iniciando ciclo visivel")
